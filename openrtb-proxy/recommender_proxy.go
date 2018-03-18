7e5409ac2b21675167ef272d3588a7afe5a3c8a4
Zdeněk Tisoň <zdenek.tison@ibillboard.com>
2018-03-18 21:43:35 +0000

    Recommender status code handled properly

--- openrtb-proxy/recommender_proxy.go
+++ openrtb-proxy/recommender_proxy.go
@@ -30,7 +30,7 @@ func NewRecommenderProxy(recommenderUrl string, recommenderTimeoutMs int, statsD
 	return &RecommenderProxy{recommenderUrl, createHTTPClient(recommenderTimeoutMs), statsDClient}
 }
 
-func (recommender *RecommenderProxy) Recommend(userId string, adRequest *AdRequest, userTargetedStatus *UserTargetedStatus, response chan RecommenderResponse) {
+func (recommender *RecommenderProxy) Recommend(userId string, adRequest *AdRequest, userTargetedStatus *UserTargetedStatus, responseChannel chan RecommenderResponse) {
 	recommender.statsDClient.Increment("requests.recommender")
 	adRequest.ProxyData = ProxyData{
 		UserId:userId,
@@ -40,30 +40,34 @@ func (recommender *RecommenderProxy) Recommend(userId string, adRequest *AdReque
 	reqBody, err := adRequest.MarshalJSON()
 	if err != nil {
 		log.Printf("Failed to serialize AdRequest %s\n", err)
-		response <- RecommenderResponse{err, nil, fasthttp.StatusNoContent}
+		responseChannel <- createErrorResponse(err)
 	}
 
-	timing := recommender.statsDClient.NewTiming()
-	res, err := recommender.httpClient.Post(recommender.RecommenderUrl, "application/json; charset=utf-8", bytes.NewReader(reqBody))
+	defer recommender.statsDClient.NewTiming().Send("time.requests.recommender")
 
+	res, err := recommender.httpClient.Post(recommender.RecommenderUrl, "application/json; charset=utf-8", bytes.NewReader(reqBody))
 	if err != nil {
 		recommender.statsDClient.Increment("error.recommender")
 
 		log.Printf("Failed to post data to Recommender %s\n", err)
-		response <- RecommenderResponse{err, nil, fasthttp.StatusNoContent}
+		responseChannel <- createErrorResponse(err)
+		return
+	}
+
+	if res.StatusCode == fasthttp.StatusNoContent {
+		responseChannel <- RecommenderResponse{nil, nil, fasthttp.StatusNoContent}
 		return
 	}
 
 	body, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	timing.Send("time.requests.recommender")
-
 	if err != nil {
 		log.Printf("Failed to read data from Recommender %s\n", err)
-		response <- RecommenderResponse{err, nil, fasthttp.StatusNoContent}
+		responseChannel <- createErrorResponse(err)
 	} else {
-		response <- RecommenderResponse{nil, body, fasthttp.StatusOK}
+		responseChannel <- RecommenderResponse{nil, body, fasthttp.StatusOK}
 	}
+
+	defer res.Body.Close()
 }
 
 func createHTTPClient(recommenderTimeoutMs int) *http.Client {
@@ -75,4 +79,8 @@ func createHTTPClient(recommenderTimeoutMs int) *http.Client {
 	}
 
 	return client
+}
+
+func createErrorResponse(err error) RecommenderResponse {
+	return  RecommenderResponse{err, nil, fasthttp.StatusNoContent}
 }
